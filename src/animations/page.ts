8b29b6cae058bc7d6124b08ad300e115782716a7
woodi97 <woodi.daily@gmail.com>
2022-07-27 19:03:32 +0900

    fix(): Page Transition Speed

--- src/animations/page.ts
+++ src/animations/page.ts
@@ -3,19 +3,19 @@ import { Variants } from 'framer-motion'
 export const pageVars: Variants = {
   hidden: (direction: number) => {
     return {
-      x: direction > 0 ? 1000 : -1000,
+      x: direction > 0 ? 1200 : -1200,
       y: 0,
       opacity: 0,
-      transition: { duration: 0.5 },
+      transition: { duration: 0.1 },
     }
   },
-  enter: { opacity: 1, x: 0, y: 0, transition: { duration: 0.5 } },
+  enter: { opacity: 1, x: 0, y: 0, transition: { duration: 0.2 } },
   exit: (direction: number) => {
     return {
-      x: direction > 0 ? -1000 : 1000,
+      x: direction > 0 ? -1200 : 1200,
       y: 0,
       opacity: 0,
-      transition: { duration: 0.5 },
+      transition: { duration: 0.4 },
     }
   },
 }
