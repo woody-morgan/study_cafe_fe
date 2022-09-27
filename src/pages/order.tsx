62edf644d82c2d19aea838af11fec276d183eb59
woody-morgan <woody.morgan.crafts@gmail.com>
2022-09-28 05:19:11 +0900

    fix(): fix compile error(happened on order page props)

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { PageLayout } from '@src/components/layout';
+import CommonHeaderContent from '@src/components/layout/PageLayout/CommonHeaderContent';
 
 const OrderPage = () => {
-  return <PageLayout fullWidth showNavigation></PageLayout>;
+  return (
+    <PageLayout fullWidth showNavigation headerContent={<CommonHeaderContent />}>
+      OrderPage
+    </PageLayout>
+  );
 };
 
 export default OrderPage;
