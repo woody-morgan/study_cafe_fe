95925761408dbac27c132c88814206e7ba522209
woodi97 <woodi.daily@gmail.com>
2022-11-17 16:29:01 +0900

    feat(): show information about cafe on index Page

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = withBundleAnalyzer({
     includePaths: [path.join(__dirname, './src/styles')],
   },
   images: {
-    domains: [],
+    domains: ['images.unsplash.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
---
+++ src/components/containers/modal/content/CafeInfoModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CafeInfoModal = () => {
+  return <div>CafeInfoModal</div>;
+};
+
+export default CafeInfoModal;
