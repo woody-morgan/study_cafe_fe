93ce87ed81ed98a4dd83c9be2200f96d75440108
woody-morgan <woody.morgan.crafts@gmail.com>
2022-12-08 13:44:54 +0900

    feat(): show cafe list on root page

---
+++ src/components/template/FavoritePageTemplate.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const FavoritePageTemplate = () => {
+  return (
+    <div className="py-8">
+      <div className="flex basis-1/2 flex-wrap justify-start gap-4">
+        {/* <FavoriteMenuCard heartFilled menu="Iced Americano" price="2000W" onHeartClick={() => {}} />
+        <FavoriteMenuCard heartFilled menu="Iced Americano" price="2000W" onHeartClick={() => {}} />
+        <FavoriteMenuCard
+          heartFilled
+          menu="Iced Americano"
+          price="2000W"
+          onHeartClick={() => {}}
+        />{' '}
+        <FavoriteMenuCard heartFilled menu="Iced Americano" price="2000W" onHeartClick={() => {}} />
+        <FavoriteMenuCard heartFilled menu="Iced Americano" price="2000W" onHeartClick={() => {}} />
+        <FavoriteMenuCard heartFilled menu="Iced Americano" price="2000W" onHeartClick={() => {}} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default FavoritePageTemplate;
--- src/core/api/apiCafe.ts
+++ src/core/api/apiCafe.ts
@@ -1,7 +1,10 @@
+import { customAxios } from '../lib/customAxios';
+
 export interface ICafe {
   id: number;
   name: string;
   description: string;
+  mainImageUrl: string;
   latitude: number;
   longitude: number;
   region: string;
@@ -9,3 +12,17 @@ export interface ICafe {
   modifiedDate: string;
   premium: boolean;
 }
+
+interface ApiGetAllCafeListRes {
+  cafeList: ICafe[];
+}
+
+export const apiGetAllCafeList = async () => {
+  try {
+    const { data } = await customAxios().get<ApiGetAllCafeListRes>(`/api/v1/cafes/list/all`);
+    return data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
